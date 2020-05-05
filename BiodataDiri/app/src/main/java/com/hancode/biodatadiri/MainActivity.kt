85d6f0d5a3dddf3c9f7836314e2a736a95fe42c5
farhannd <fnafis96@gmail.com>
2020-05-05 21:25:06 +0700

    Simpan input sudah tidak error

--- BiodataDiri/app/src/main/java/com/hancode/biodatadiri/MainActivity.kt
+++ BiodataDiri/app/src/main/java/com/hancode/biodatadiri/MainActivity.kt
@@ -3,19 +3,64 @@ package com.hancode.biodatadiri
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    //
+    private var namaInput : String = ""
+    private var emailInput : String = ""
+    private var telpInput : String = ""
+    private var alamatInput : String = ""
+    private var genderInput : String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btnSave.setOnClickListener{goToProfilActivity()}
+        setDataSpinnerGender()
+        btnSave.setOnClickListener{validasiInput()}
     }
 
+    private fun setDataSpinnerGender() {
+        val adapter = ArrayAdapter.createFromResource(this,R.array.jenis_kelamin, android.R.layout.simple_spinner_dropdown_item)
+        SpinnerGender.adapter = adapter
+    }
+
+
+    //input validasi
+    private fun validasiInput() {
+        namaInput = edtNama.text.toString()
+        emailInput = edtEmail.text.toString()
+        telpInput = edtTelp.text.toString()
+        alamatInput = edtAddress.text.toString()
+        genderInput = SpinnerGender.selectedItem.toString()
+        when{
+            namaInput.isEmpty() -> edtNama.error = "Nama tidak boleh kosong"
+            genderInput.equals("Pilih Jenis Kelamin", ignoreCase = true) -> tampilToast ("Jenis Kelamin harus dipilih")
+            emailInput.isEmpty() -> edtEmail.error = "Email tidak boleh kosong"
+            telpInput.isEmpty() -> edtTelp.error = "Telp tidak boleh kosong"
+            alamatInput.isEmpty() -> edtAddress.error = "Alamat tidak boleh kosong"
+            else -> {
+                tampilToast ("Navigasi ke halaman profil")
+                goToProfilActivity()
+            }
+    }
+}
+    private fun tampilToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+//fungsi pindah ke ProfilAct dan indput data
     private fun goToProfilActivity() {
         val intent = Intent(this, ProfilActivity::class.java)
+        val bundle = Bundle()
+        bundle.putString("nama", namaInput)
+        bundle.putString("gender", genderInput)
+        bundle.putString("email", emailInput)
+        bundle.putString("telp", telpInput)
+        bundle.putString("alamat", alamatInput)
+        intent.putExtras(bundle)
         startActivity(intent)
     }
-    private var namaInput : String ="*"
 }
--- BiodataDiri/app/src/main/java/com/hancode/biodatadiri/ProfilActivity.kt
+++ BiodataDiri/app/src/main/java/com/hancode/biodatadiri/ProfilActivity.kt
@@ -2,6 +2,7 @@ package com.hancode.biodatadiri
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_profil.*
 
 
 class ProfilActivity : AppCompatActivity() {
@@ -9,5 +10,21 @@ class ProfilActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profil)
+
+        ambilData()
+    }
+    private fun ambilData(){
+        val bundle = intent.extras
+
+        val nama = bundle!!.getString("nama")
+        val gender = bundle.getString("gender")
+        val email = bundle.getString("email")
+        val telp = bundle.getString("telp")
+        val alamat = bundle.getString("alamat")
+        txtName.text = nama
+        txtGender.text = gender
+        txtEmail.text = email
+        txtTelp.text = telp
+        txtAddress.text = alamat
     }
 }
