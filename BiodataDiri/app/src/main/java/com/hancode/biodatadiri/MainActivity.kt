0a2f2a2ece2be8b1d272d871fd750fe8a6098830
farhannd <fnafis96@gmail.com>
2020-05-05 20:16:31 +0700

    no message

--- BiodataDiri/app/src/main/java/com/hancode/biodatadiri/MainActivity.kt
+++ BiodataDiri/app/src/main/java/com/hancode/biodatadiri/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.hancode.biodatadiri
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnSave.setOnClickListener{goToProfilActivity()}
     }
+
+    private fun goToProfilActivity() {
+        val intent = Intent(this, ProfilActivity::class.java)
+        startActivity(intent)
+    }
+    private var namaInput : String ="*"
 }
--- BiodataDiri/app/src/main/java/com/hancode/biodatadiri/ProfilActivity.kt
+++ BiodataDiri/app/src/main/java/com/hancode/biodatadiri/ProfilActivity.kt
@@ -3,6 +3,7 @@ package com.hancode.biodatadiri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 class ProfilActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
