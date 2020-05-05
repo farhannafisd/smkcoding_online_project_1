49c3c0653b113bc410ea17892df3b158289faa05
farhannd <fnafis96@gmail.com>
2020-05-05 09:45:48 +0700

    Activity Profil

---
+++ BiodataDiri/app/src/main/java/com/hancode/biodatadiri/ProfilActivity.kt
@@ -0,0 +1,12 @@
+package com.hancode.biodatadiri
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfilActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profil)
+    }
+}
